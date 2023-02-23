27d878879bd64a2d5dbe481088d1fb609f28ef6e
NhanBui <buiducnhan15122003@gmail.com>
2023-02-23 21:38:46 +0700

    NhanBui 9.39PM commit

---
+++ LamViecVoiFile_Form/DIEM.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class DIEM
+    {
+        private double ToanCC;
+        private double LyDC;
+        private double HoaDC;
+
+        public DIEM()
+        {
+            //throw new System.NotImplementedException();
+        }
+
+        public DIEM(double ToanCC, double LyDC, double HoaDC)
+        {
+            //throw new System.NotImplementedException();
+            this.ToanCC = ToanCC;
+            this.LyDC = LyDC;
+            this.HoaDC = HoaDC;
+        }
+
+        public double TCC
+        {
+            get => ToanCC;
+            set
+            {
+                ToanCC = value;
+            }
+        }
+
+        public double LDC
+        {
+            get => LyDC;
+            set
+            {
+                LyDC = value;
+            }
+        }
+        public double HDC
+        {
+            get => HoaDC;
+            set
+            {
+                HoaDC = value;
+            }
+        }
+        public double TinhDTB()
+        {
+            //throw new System.NotImplementedException();
+            double dtb = (HoaDC + LyDC + ToanCC) / 3;
+            return dtb;
+        }
+    }
+}
---
+++ LamViecVoiFile_Form/SinhVien.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class SinhVien
+    {
+        private string name;
+        private int mssv;
+        private DIEM diem;
+        private double gpa;
+
+        public SinhVien()
+        {
+            //throw new System.NotImplementedException();
+            name = "NULL";
+            mssv = 0;
+            diem = new DIEM(0, 0, 0);
+            gpa = diem.TinhDTB();
+        }
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+            }
+        }
+        public int Mssv
+        {
+            get => mssv;
+            set
+            {
+                mssv = value;
+            }
+        }
+        public DIEM Diem
+        {
+            get => diem;
+            set
+            {
+                diem = value;
+            }
+        }
+        public double Gpa
+        {
+            get => diem.TinhDTB();
+        }
+        public static string ToStringGpa(double grade)
+        {
+            return ($"{grade:F2}");
+        }
+    }
+}
